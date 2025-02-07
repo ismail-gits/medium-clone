78bed7a514d88e18e6f73646b232fc41d041fc52
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-02-08 05:28:00 +0530

    update validation.ts

--- backend/src/validation.ts
+++ backend/src/validation.ts
@@ -11,5 +11,23 @@ export const signinSchema = z.object({
   password: z.string()
 })
 
+export const createBlogSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+  thumbnail: z.string().optional(),
+})
+
+export const updateBlogSchema = z.object({
+  id: z.string(),
+  title: z.string().optional(),
+  content: z.string().optional(),
+  thumbnail: z.string().optional(),
+})
+
+export const idSchema = z.string()
+
 export type signupType = z.infer<typeof signupSchema> 
 export type signinType = z.infer<typeof signinSchema>
+export type createBlogType = z.infer<typeof createBlogSchema>
+export type updateBlogtype = z.infer<typeof updateBlogSchema>
+export type idType = z.infer<typeof idSchema>
